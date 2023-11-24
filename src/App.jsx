cc2d716ad8c4e7e4e29b853fd0b1cdff7f98b8f4
iyashraj <yashr3037@gmail.com>
2023-11-24 21:23:02 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MainComp from './components/MainComp'
+
+function App() {
+
+  return (
+    <>
+      <MainComp />
+    </>
+  )
+}
+
+export default App
