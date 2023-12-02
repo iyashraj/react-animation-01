1f87521b655f9b526cebd4a573e34a1ef3f06e8f
iyashraj <yashr3037@gmail.com>
2023-12-02 10:34:31 +0530

    final push to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./app.css";
 import { Canvas } from "@react-three/fiber";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Canvas>
+    <Canvas style={{height:"100vh"}}>
       <App />
     </Canvas>
   </React.StrictMode>
